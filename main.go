76b413b5d614df2d4434dc3a3fa37482c05c3138
swissdeam <mpd2000@mail.ru>
2022-03-08 13:49:58 +0400

    base of pointers

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// pointers
+
 func main() {
-	fmt.Println("hello")
+	x := "value of x"
+	a := &x         // & means where "a" take the data; in this case "a" takes data from "x"
+	fmt.Println(a)  // without "*", "a" equals addres of "x"
+	fmt.Println(*a) // with "*", "a" means value of source(in this case source is "x")
+
 }
